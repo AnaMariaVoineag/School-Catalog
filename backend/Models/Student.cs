4dedbc64a6200f28fa52bd60afa786c2e7564681
cristi456 <37697268+cristi456@users.noreply.github.com>
2025-03-23 15:02:35 +0200

    MEOWZER-50

--- backend/Models/Student.cs
+++ backend/Models/Student.cs
@@ -6,6 +6,5 @@ public class Student
     public int UserId { get; set; }
     public User User { get; set; }
 
-    // Many-to-Many with Course (handled via Fluent API)
     public List<Course> EnrolledCourses { get; set; } = new List<Course>();
 }
