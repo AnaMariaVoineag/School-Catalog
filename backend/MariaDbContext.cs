ab9834276a0daa3bc049a1fd8bbb78bc047bfe17
elianel <xElianChan@protonmail.com>
2025-03-23 15:15:25 +0200

    MEOWZER-78 add initial mariadb db context

---
+++ backend/MariaDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace backend
+{
+    public class MariaDbContext : DbContext
+    {
+        public MariaDbContext(DbContextOptions<MariaDbContext> options)
+            : base(options) { }
+
+    }
+}
